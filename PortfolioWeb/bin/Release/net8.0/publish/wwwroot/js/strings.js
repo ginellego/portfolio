1b5c58863e6b99ef5fefbd007d33d9fd66d2863e
ginellego <ginelletemull@gmail.com>
2026-02-22 17:41:19 -0600

    changes for Azure deployment instead of Vercel

---
+++ PortfolioWeb/bin/Release/net8.0/publish/wwwroot/js/strings.js
@@ -0,0 +1,30 @@
+
+window.siteStrings = {
+    about: "Hello! I'm Ginelle, a passionate software developer with a love for creating innovative solutions. With a background in computer science and experience in various programming languages, I enjoy tackling complex problems and building user-friendly applications.\n\nI like to be involved in the community by volunteering for organizations like Manitoba Mutts and Pride Winnipeg.\n\nWhen I'm not coding, you can find me decorating my Animal Crossing island, crocheting, reading, working out or spending time with my cat, Poe.",
+    home: "Welcome to my portfolio website! Here, you'll find a collection of my projects, skills, and experiences in the world of software development. Feel free to explore and learn more about my journey as a developer.",
+    portfolio: "Here are some of the projects I've worked on. Each project showcases my skills and passion for software development. Click on each project to learn more about the technologies used and the challenges I overcame during development.",
+    contact: "If you'd like to get in touch with me, please feel free to reach out! You can contact me on LinkedIn. I'm always open to discussing new opportunities, collaborations, or just connecting with fellow developers. Looking forward to hearing from you!",
+    project1: "Volunteer Hub App", 
+    project1sub: "Manitoba Mutts Dog Rescue - Present",
+    project1text: "Designing and developing a cross-platform application to aid the volunteer team in communication, support and collaboration by providing a dedicated centre for volunteer information, alerts, chats, and post sharing.",
+    project1link: "https://github.com/ginellego/MMDR-app",
+    project2: "Finance Management Web App",
+    project2sub: "COMP 4350 Group Project - 2025",
+    project2text: "An app to ease the task of personal financial analysis with dashboard, analytics, transactions, goals, and bank connection pages as a full-stack contributor integrating APIs and endpoints to enhance web flow and user experience.",
+    project2link: "https://github.com/Money-Lens/Money-Lens-App",
+    project3: "Event Planning Android App",
+    project3sub: "COMP 3350 Group Project - 2024",
+    project3text: "Features include sign in/sign up/sign out, profile creation for event planners and vendors, event creation, invoice generation and more.",
+    project3link: "https://github.com/ginellego/EventEase",    
+    project4: "Contractor Search Web App",
+    project4sub: "COMP 3020 Group Project - 2023",
+    project4text: "A web application that allows users to search for contractors in their area based on their skills and location. Users can leave reviews and project status updates for others to view in a feed.",
+    project4link: "https://github.com/ginellego/Contractor_Website",
+    project5: "Lego Database CLI Query Program",
+    project5sub: "COMP 3380 Group Project - 2023",
+    project5text: "A command-line interface program that allows users to query a database of Lego sets and parts using SQL queries.",
+    project5link: "https://github.com/ginellego/Lego_Database",
+
+
+
+};
--- PortfolioWeb/obj/Release/net8.0/PortfolioWeb.AssemblyInfo.cs
+++ PortfolioWeb/obj/Release/net8.0/PortfolioWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb72c609f330a4a1d9721f0576fc4aa49f1fed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cb359549573948411a05b6339707c63ab9f5f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
