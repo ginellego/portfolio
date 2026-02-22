188c85d358a8e2e38117c782ff11a0133642f3fd
ginellego <ginelletemull@gmail.com>
2026-02-21 21:50:14 -0600

    initial portfolio commit

---
+++ PortfolioWeb/Pages/Portfolio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWeb.Pages;
+
+public class PortfolioModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
