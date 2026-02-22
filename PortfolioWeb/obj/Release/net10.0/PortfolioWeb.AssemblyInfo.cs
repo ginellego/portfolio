adadba5d06020cd8d5888ac85c93d2b46bc62445
ginellego <ginelletemull@gmail.com>
2026-02-21 22:08:36 -0600

    create publish directory for deployment

--- PortfolioWeb/obj/Release/net10.0/PortfolioWeb.AssemblyInfo.cs
+++ PortfolioWeb/obj/Release/net10.0/PortfolioWeb.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188c85d358a8e2e38117c782ff11a0133642f3fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
